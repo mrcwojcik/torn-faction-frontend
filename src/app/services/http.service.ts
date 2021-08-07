7ae86254f9aed0c4e29b02acd2c0ddf1b12df098
Marcin <maethi@gmail.com>
2021-08-07 11:56:29 +0200

    Set environment data

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -2,32 +2,35 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Faction, Member, Event} from "../app.component";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
+  private API_URL = environment.apiUrl;
+
   constructor(private http: HttpClient) { }
 
   getFaction(): Observable<Faction>{
-    return this.http.get<Faction>('http://localhost:8081/api/faction');
+    return this.http.get<Faction>(this.API_URL + '/api/faction');
   };
 
   getMembers(): Observable<Array<Member>>{
-    return this.http.get<Array<Member>>('http://localhost:8081/api/members');
+    return this.http.get<Array<Member>>(this.API_URL + '/api/members');
   };
 
   addEvent(event: Event): Observable<Event>{
-    return this.http.post<Event>('http://localhost:8081/api/event/add', event);
+    return this.http.post<Event>(this.API_URL + '/api/event/add', event);
   }
 
   getEventList(): Observable<Array<Event>>{
-    return this.http.get<Array<Event>>('http://localhost:8081/api/events');
+    return this.http.get<Array<Event>>(this.API_URL + '/api/events');
   }
 
   deleteOldEvents(): Observable<Array<Event>>{
-    return this.http.get<Array<Event>>('http://localhost:8081/api/events/clear');
+    return this.http.get<Array<Event>>(this.API_URL + '/api/events/clear');
   }
 
 }
